f868e6b945a7cc9c39eb840c7f326c4f86017cc0
AvaTuttle <Travis.Tuttle@sdgku.edu>
2024-11-09 12:50:38 -0700

    more server stuff

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 import json 
 
 app = Flask(__name__)
@@ -33,5 +33,48 @@ def greet(name):
 @app.get("/firewall")
 def firewall_message():
     return "<h1 style=color:red>Access Denied</h1><p>Your IP has been restricted by the firewall. Please contact the administrator if you believe this is an error.</p>", 403
+
+# #############
+products = []
+@app.get("/api/products")
+def get_products():
+    return json.dumps(products)
+
+@app.post("/api/products")
+def save_products():
+    item = request.get_json()
+    print(item)
+    products.append(item)
+    return json.dumps(item)
+
+@app.put("/api/products/<int:index>")
+def update_products(index):
+    updated_item=request.get_json()
+    if 0<= index <=len(products):
+        products[index] = updated_item
+        return json.dumps(updated_item)
+    else:
+        return "that index does not exist"
+    
+
+@app.delete("/api/products/<int:index>")
+def delete_products(index):
+    if 0<= index <=len(products):
+        delete_item = products.pop(index)
+        return json.dumps(delete_item)
+    else:
+        return "that index does not exist"
+    
+# patch -- the method to update a specific element into python is: .update
+
+@app.patch("/api/products/<int:index>")
+def update_item(index):
+    if 0 <= index <= len(products):
+        updated_field = request.get_json()
+        products(index).update(updated_field)
+        return json.dumps(updated_field)
+    else:
+        return "That index does not exist"
+
 
 app.run(debug=True)
